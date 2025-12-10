ff920e73b5ef76f6aa2f2767d38676f1959e28b1
Gajanan Bidwai <gajananbidwai525@gmail.com>
2025-12-10 14:40:50 +0530

    Initial commit

---
+++ GraphicsAndAnimation/GraphicsAndAnimation/GraphicsAndAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphicsAndAnimationApp.swift
+//  GraphicsAndAnimation
+//
+//  Created by Neosoft on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GraphicsAndAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
