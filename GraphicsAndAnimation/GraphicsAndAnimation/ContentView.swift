76e104355d9b8baceeedeba205d916fb8fa5ac7a
Gajanan Bidwai <gajananbidwai525@gmail.com>
2025-12-10 15:23:11 +0530

    All shapes

--- GraphicsAndAnimation/GraphicsAndAnimation/ContentView.swift
+++ GraphicsAndAnimation/GraphicsAndAnimation/ContentView.swift
@@ -8,12 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isActive: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ScrollView(.horizontal,showsIndicators: false) {
+                HStack() {
+                    Rectangle()
+                        .frame(width: 100, height: 100)
+                    RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
+                        .frame(width: 100, height: 100)
+                    Circle()
+                        .frame(width: 100, height: 100)
+                    Ellipse()
+                        .frame(width: 100, height: 50)
+                    Capsule()
+                        .frame(width: 100, height: 50)
+                }
+            }
+            .padding()
+            
+            Button(action: {
+                isActive.toggle()
+            }, label: {
+                Text(isActive ? "Active" : "InActive")
+                    .foregroundStyle(.white)
+                    .font(.system(size: 24, weight: .bold, design: .default))
+            })
+            
+            .frame(width: 200, height: 50)
+            .background(isActive ? Color.green : Color.red, in: RoundedRectangle(cornerSize: CGSize(width: 25, height: 25)))
+            
+            
         }
         .padding()
     }
